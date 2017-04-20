bf87cdb780afd19aca16029585effce02cacab88
Jakub Petriska <jakubpetriska@gmail.com>
2017-04-20 20:22:50 +0200

    ctypes testing

--- player_lib/__init__.py
+++ player_lib/__init__.py
@@ -1,1 +1,6 @@
-from player_lib.player_lib import *
+from ctypes import *
+# TODO make path relative
+lib = CDLL('/home/jakub/Projects/acpc-poker-bot-python-wrapper/player_lib/player_lib.so')
+# c = a.fuckIt()
+
+# from player_lib.player_lib import *
