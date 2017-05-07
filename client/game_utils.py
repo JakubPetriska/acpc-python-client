403b3c7e55b0ca6639d3327fb50e744d49a4e4d0
Jakub Petriska <jakubpetriska@gmail.com>
2017-05-07 11:38:40 +0200

    Add game_utils documentation

--- client/game_utils.py
+++ client/game_utils.py
@@ -2,8 +2,26 @@ _MAX_SUITS = 4
 
 
 def card_rank(card):
+    """Returns card rank from card's int representation.
+
+    Args:
+        card (int): Int representation of the card. This representation
+                    has each card rank of each suit represented by unique integer.
+
+    Returns:
+        int: Card rank as 0 based index.
+    """
     return card // _MAX_SUITS
 
 
 def card_suit(card):
+    """Returns card suit from card's int representation.
+
+    Args:
+        card (int): Int representation of the card. This representation
+                    has each card rank of each suit represented by unique integer.
+
+    Returns:
+        int: Card suit as 0 based index.
+    """
     return card % _MAX_SUITS
