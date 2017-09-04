de45884d33357d9a4267c0ba3ada593b74eff986
Jakub Petriska <jakubpetriska@gmail.com>
2017-09-04 14:19:48 +0200

    Add method to generate game's deck

--- acpc_python_client/game_utils.py
+++ acpc_python_client/game_utils.py
@@ -1,4 +1,5 @@
 _MAX_SUITS = 4
+_MAX_RANKS = 13
 
 
 def card_rank(card):
@@ -25,3 +26,11 @@ def card_suit(card):
         int: Card suit as 0 based index.
     """
     return card % _MAX_SUITS
+
+
+def generate_deck(game):
+    deck = []
+    for suit in range(_MAX_SUITS - game.get_num_suits(), _MAX_SUITS):
+        for rank in range(_MAX_RANKS - game.get_num_ranks(), _MAX_RANKS):
+            deck.append(rank * _MAX_SUITS + suit)
+    return deck
